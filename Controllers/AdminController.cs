ec38106641d5e904775e27b009a1a39af6a79de3
DESKTOP-KA3UFEA\Citrus_0974_ThinkPad <citrus_lumia@outlook.com>
2026-01-17 09:03:53 +0400

    news_crud_view_added

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -6,10 +6,12 @@ namespace CitrusMicroblog.Controllers
     public class AdminController : Controller
     {
         private IFormMessageRepository _repository;
+        private readonly INewsRepository _repository1;
 
-        public AdminController(IFormMessageRepository repository)
+        public AdminController(IFormMessageRepository repository, INewsRepository rep1)
         {
             _repository = repository;
+            _repository1 = rep1;
         }
         public ViewResult FormMessagesList()
         {
@@ -19,6 +21,11 @@ namespace CitrusMicroblog.Controllers
         public ViewResult Index()
         {
             return View();
+        }
+
+        public ViewResult NewsList()
+        {
+            return View(_repository1.NewsTopics.Reverse());
         }
     }
 }
